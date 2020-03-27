619ac65aeba93971aadf32c54354f1a3cc91ebf2
GuilhermeAugustini <guilherme.maugustini@gmail.com>
2020-03-27 15:05:10 -0300

    Innitial Commit

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar uma informação no back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizando para criar ou alterar recursos
+ */
+
+
